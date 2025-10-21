5b45395f6bdf5d068f8d84ae05ea3b0fa5cbcab5
seb <shumatesebastian@gmail.com>
2025-10-21 09:20:59 -0400

    Added login and register routes

--- backend/main.py
+++ backend/main.py
@@ -74,6 +74,15 @@ def add_song():
         return redirect(url_for('index'))
     return render_template('add_song.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
 #Run The App
 if __name__ == '__main__':
     app.run(debug=True)
