8b4aceed391f29ecf052e8df0857114bab12e7f2
FinleyClapper <fc56@uakron.edu>
2025-10-20 20:08:58 -0400

    Fix kaggle import

--- backend/api.py
+++ backend/api.py
@@ -1,16 +1,18 @@
 from flask import Flask, jsonify, request
 from sqlalchemy import *
 import pandas as pd
-#config flask an db
+import kagglehub
+path = kagglehub.dataset_download("maharshipandya/-spotify-tracks-dataset")
 app = Flask(__name__)
-df = pd.read_csv("data/SpotifyTracks.csv")
+df = pd.read_csv(f'{path}/dataset.csv')
 eng = create_engine("sqlite:///spotify.db")
 df.to_sql("tracks", con=eng, if_exists="replace", index=False)
 
 @app.route("/search/title")
 def search_title():
     title = request.args.get("title", "").strip()
-    querry = text("SELECT * FROM tracks WHERE track_name LIKE :title")
+    querry = text("SELECT * FROM tracks WHERE track_name LIKE :title LIMIT 20")
+    print()
     params = {
         "title": f"%{title}%"
     }
@@ -19,16 +21,16 @@ def search_title():
 @app.route("/search/artist")
 def search_artist():
     artist = request.args.get("artist", "").strip()
-    querry = text("SELECT * FROM tracks WHERE artists=:artist")
+    querry = text("SELECT * FROM tracks WHERE artists=:artist LIMIT 20")
     params = {
         "artist": f"%{artist}%"
     }
     songs = pd.read_sql(querry,eng,params=params)
     return jsonify(songs.to_dict(orient="records"))
 @app.route("/search/genre")
-def search_artist():
+def search_genre():
     genre = request.args.get("genre", "").strip()
-    querry = text("SELECT * FROM tracks WHERE track_genre=:genre")
+    querry = text("SELECT * FROM tracks WHERE track_genre=:genre LIMIT 20")
     params = {
         "genre": f"%{genre}%"
     }
@@ -37,3 +39,4 @@ def search_artist():
 @app.route("/edit/genre")
 def edit_genre():
     pass
+app.run(debug=True, port=5000)
--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import pandas as pd
